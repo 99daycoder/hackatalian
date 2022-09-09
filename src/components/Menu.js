ba27001785e4d25efed8bb80b30e2ffc7f2028f3
LunaChong <potterwo123@gmail.com>
2022-09-09 14:01:19 +0100

    add flex

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import NavBar from './NavBar';
 import foodData from '../data/data.js';
 import FoodItem from './FoodItem';
+import { Flex, Spacer } from '@chakra-ui/react'
 
 export default function Menu() {
   return (
